039cf99f38e4ac332d10e238377c400ecc8b45d2
井上俊博 <t.artwork.ceramica@gmail.com>
2022-06-07 09:14:39 +0900

    filter-function adding adjust

--- filter_function/filter_function.js
+++ filter_function/filter_function.js
@@ -12,109 +12,128 @@ const vFilter = Vue.createApp ({
       "saturate",
       "sepia"
     ],
+    // outputValue: "",
     selectedValue: "",
     sliderValues: {},
-    outputValue: "",
-    // output: "",
-    // image: "",
-    // code: "",
   }),
   created: function () {
-    // console.log('created: ');
   },
 
   mounted: function () {
-    // this.selectedValue = this.$refs.select.value;
-    // this.sliderValues = {
-    //   value: this.$refs.slider.value,
-    //   min: this.$refs.slider.min,
-    //   max: this.$refs.slider.max,
-    //   step: this.$refs.slider.step,
-    // };
-    // let slider = this.$refs.slider;
-
-    console.log(this.sliderValues);
-    console.log(this.sliderValues['value']);
-    this.setOutput();
-
-    // this.outputText = this.$refs.output;
-
-    // console.log(output);
-    // console.log(output.textContent);
-    // let image = this.$refs.image;
-    // let code = this.$refs.code;
+    this.setSlider(this.$refs.select.value);
+    this.setImage(this.$refs.select.value);
   },
 
   beforeUpdated: function () {
-    console.log('updated: ');
-    // select.addEventListener('change', () => {
-    //   setSlider(select.value);
-    //   setDiv(select.value);
-    //   console.log('updated: ');
-    // });
+    
   },
   
 
   methods: ({
 
+    clickHandler: function () {
+      console.log(`${this.$refs.image.style}`);
+      console.log(this.$refs.image.style);
+      console.log(this.$refs.select.value);
+
+      this.$refs.image.style.filter = this.$refs.select.value + 
+      '(' + this.$refs.slider.value + this.$refs.slider.getAttribute('data-unit') + ')' ;
+      
+
+      console.log(this.$refs.image.style.filter);
+      // console.log(this.$refs.slider.value);
+      // console.log(this.$refs.output.textContent);
+    },
+
     selectFilterFunction: function () {
-      console.log(this.select);
       this.setSlider(this.$refs.select.value);
-      this.setOutput();
+      this.setImage(this.$refs.select.value);
     },
 
     // どのフィルター関数でinput[type="range"]を表示させるか
     setSlider: function (filter) {
       if (filter === 'blur') {
-        this.sliderValues['value'] = 0;
-        this.sliderValues['min'] = 0;
-        this.sliderValues['max'] = 30;
-        this.sliderValues['value'] = 1;
+        this.$refs.slider.value = 0;
+        this.$refs.slider.min = 0;
+        this.$refs.slider.max = 30;
+        this.$refs.slider.step = 1;
         this.$refs.slider.setAttribute('data-unit','px');
+      //基準値が1のグループ
       } else if (filter === 'brightness' || filter === 'contrast' || filter === 'saturate') {
-        slider.value = 1;
-        slider.min = 0;
-        slider.max = 4;
-        slider.step = 0.05;
-        slider.setAttribute('data-unit','');
-      } else if (filter === 'drop-shadow') {
-        slider.value = 0;
-        slider.min = -20;
-        slider.max = 40;
-        slider.step = 1;
-        slider.setAttribute('data-unit','px');
+        this.$refs.slider.value = 1;
+        this.$refs.slider.min = 0;
+        this.$refs.slider.max = 4;
+        this.$refs.slider.step = 0.05;
+        this.$refs.slider.setAttribute('data-unit','');
+
+      // 基準を1とした 0 ~ 1の範囲で指定
       } else if (filter === 'opacity') {
-        slider.value = 1;
-        slider.min = 0;
-        slider.max = 1;
-        slider.step = 0.01;
-        slider.setAttribute('data-unit','');
+        this.$refs.slider.value = 1;
+        this.$refs.slider.min = 0;
+        this.$refs.slider.max = 1;
+        this.$refs.slider.step = 0.01;
+        this.$refs.slider.setAttribute('data-unit','');
+
+      // 基準を0とした 0 ~ 1の範囲で指定
       } else if (filter === 'grayscale' || filter === 'invert' || filter === 'sepia') {
-        slider.value = 0;
-        slider.min = 0;
-        slider.max = 1;
-        slider.step = 0.01;
-        slider.setAttribute('data-unit','');
+        this.$refs.slider.value = 0;
+        this.$refs.slider.min = 0;
+        this.$refs.slider.max = 1;
+        this.$refs.slider.step = 0.01;
+        this.$refs.slider.setAttribute('data-unit','');
+
+      //それ以外のグループ
+      } else if (filter === 'drop-shadow') {
+        this.$refs.slider.value = 0;
+        this.$refs.slider.min = -20;
+        this.$refs.slider.max = 40;
+        this.$refs.slider.step = 1;
+        this.$refs.slider.setAttribute('data-unit','px');
       } else if (filter === 'hue-rotate') {
-        slider.value = 0;
-        slider.min = 0;
-        slider.max = 360;
-        slider.step = 1;
-        slider.setAttribute('data-unit','deg');
+        this.$refs.slider.value = 0;
+        this.$refs.slider.min = 0;
+        this.$refs.slider.max = 360;
+        this.$refs.slider.step = 1;
+        this.$refs.slider.setAttribute('data-unit','deg');
       }
     },
 
     setImage: function (filter) {
+      // imageにfilterプロパティの設定
+      // filter: xxxx; の xxxx を 宣言する、 例. filter: blur(8px);
+      //
       if (filter === 'drop-shadow') {
         
+        console.log("shadow");
+        // 
+        // this.$refs.image.style.filter = `${this.$refs.select.value}
+        // (${Math.round(this.$refs.slider.value)}${this.$refs.slider.getAttribute('data-unit')}
+        // ${Math.round(this.$refs.slider.value)}${this.$refs.slider.getAttribute('data-unit')}
+        // ${Math.round(Math.abs(this.$refs.slider.value/2))}${this.$refs.slider.getAttribute('data-unit')})`;
+
+        this.$refs.image.style.filter = this.$refs.select.value
+        '(' + Math.round(this.$refs.slider.value) + this.$refs.slider.getAttribute('data-unit') + ' ' +
+        Math.round(this.$refs.slider.value) + this.$refs.slider.getAttribute('data-unit') + ' ' +
+        Math.round(Math.abs(this.$refs.slider.value/2)) + this.$refs.slider.getAttribute('data-unit') + ')';
+      } else {
+        this.$refs.image.style.filter = this.$refs.select.value + 
+      '(' + this.$refs.slider.value + this.$refs.slider.getAttribute('data-unit') + ')' ;
+        // this.$refs.image.style.filter = `${this.$refs.select.value}
+        // (${this.$refs.slider.value}${this.$refs.slider.getAttribute('data-unit')})`;
       }
 
-      // this.output.textContent = slider.value;
+      this.updateOutput();
+      this.updateCode();
+      //// this.output.textContent = slider.value;
     },
 
-    setOutput: function () {
-      this.outputValue = this.sliderValues['value'];
+    updateOutput: function() {
+      this.$refs.output.textContent = this.$refs.slider.value;
     },
+
+    updateCode: function() {
+      this.$refs.code.textContent = `filter: ${this.$refs.image.style.filter}`;
+    }
 
     
   }),
